156a73fcde86d0e9582f7f2867861148b27a5408
Niraj Karad <nirajk@glam.com>
2013-12-11 16:28:41 +0530

    Updating Testcases

--- src/test/java/com/practise/AppTest.java
+++ src/test/java/com/practise/AppTest.java
@@ -19,90 +19,88 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+
+	private Injector inj;
+	private CustomerDB c_db;
+	private Gateway gw;
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		System.setProperty("db_name", "visa");
+		
+		inj = Guice.createInjector(new TestModule());
+		c_db = inj.getInstance(CustomerDB.class);
+		gw = inj.getInstance(Gateway.class); 
+	}
+	
+/*    public AppTest( String testName )
     {
         super( testName );
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
-
-    /**
-     * Rigourous Test :-)
-     */
     
     public void testApp()
     {
-		
 	    assert(true);
-    }
+    }*/
     
     
     public void testCustomer1(){
-    	System.setProperty("db_name", "visa");
+    	
     	//Start: 1st Customer
 		Customer c1 = new Customer("Raj", "4", "21", "Pune");
+		
+		assertNotNull("Checking Customer 1 ", c1);
+		
 		List<Item> itm1 = new LinkedList<Item>();
 		Item i1 = new Item("1", "Tshirt", "Nike Brand", 599);
 		Item i2 = new Item("2", "Shoes", "Puma Brand", 499);
 		Item i3 = new Item("3", "Jacket", "Woodsland Brand", 699);
 		Item i4 = new Item("4", "Watch", "Titan Brand", 599);
+		assertEquals(0, itm1.size());
 		itm1.add(i1);
+		assertEquals(1, itm1.size());
 		itm1.add(i2);
+		assertEquals(2, itm1.size());
 		itm1.add(i3);
+		assertEquals(3, itm1.size());
 		itm1.add(i4);
-		//End: 1st Customer
-			
-		Injector inj = Guice.createInjector(new TestModule());
-		CustomerDB c_db = inj.getInstance(CustomerDB.class);
-		Gateway gw = inj.getInstance(Gateway.class); 
+		assertEquals(4, itm1.size());
 		
-		//For 1st Customer
 		c_db.save(c1.getCust_id(), c1);
 		gw.makePayment(c1.getCust_id(),itm1,"debit");
-		float amount1 = gw.makeBill();
 		
-		assertEquals( 1896, amount1, 0.00);
+		assertEquals( 1896,  gw.makeBill(), 0.00);
     }
     
     public void testCustomer2(){
-    	System.setProperty("db_name", "visa");
-    	
     	Customer c2 = new Customer("Ravi", "4", "21", "Pune");
+    	assertNotNull("Checking Customer 2 ", c2);
 		List<Item> itm2 = new LinkedList<Item>();
 		Item i11 = new Item("1", "Tshirt", "Nike Brand", 999);
 		Item i22 = new Item("2", "Shoes", "Puma Brand", 1599);
 		Item i33 = new Item("3", "Jacket", "Woodsland Brand", 1799);
 		Item i44 = new Item("4", "Watch", "Titan Brand", 5899);
+		assertEquals(0, itm2.size());
 		itm2.add(i11);
+		assertEquals(1, itm2.size());
 		itm2.add(i22);
+		assertEquals(2, itm2.size());
 		itm2.add(i33);
+		assertEquals(3, itm2.size());
 		itm2.add(i44);
-		//End: 2nd Customer
+		assertEquals(4, itm2.size());
 		
-		Injector inj = Guice.createInjector(new TestModule());
-		CustomerDB c_db = inj.getInstance(CustomerDB.class);
-		Gateway gw = inj.getInstance(Gateway.class); 			
-		
-		//For 2nd Customer
 		c_db.save(c2.getCust_id(), c2);
 		gw.makePayment(c2.getCust_id(),itm2,"visa");
-		float amount2 = gw.makeBill();
 		
-		assertEquals( 8796, amount2, 0.00);
+		assertEquals( 8796, gw.makeBill(), 0.00);
     }
     
 }
